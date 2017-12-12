24fcf68f857f8b4a94353c31b2c8934aef7ae93e
LeeShannon <leeshannong@hotmail.com>
2017-12-12 10:59:07 +0200

    section 3

---
+++ Section 3/v-for.js
@@ -0,0 +1,10 @@
+new Vue({
+	el: '#app',
+  data: {
+  	ingredients: ['muffin mix', 'blueberries', 'eggs', 'milk'],
+    people: [
+    	{name: 'Beeba', age: 23},
+      {name: 'Babe-ba', age: 23}
+    ]
+  }
+});
