1b8d32ba9965084e5f45edb512b3a6ef6a1bc4e6
LeeShannon <leeshannong@hotmail.com>
2017-12-27 10:28:01 +0200

    first vue project

---
+++ Projects/Setup/app.js
@@ -0,0 +1,51 @@
+new Vue({
+  el: '#app',
+  data: {
+    gameIsRunning: false,
+    playerHP: 100,
+    monsterHP: 100,
+    playerDmg: 0,
+    monsterDmg: 0,
+    playerHeal: 0,
+    didPlayerHeal: false,
+    Healed: false,
+    log: []
+  },
+  methods: {
+    startGame: function(){
+      this.gameIsRunning = true;
+      this.playerHP = 100;
+      this.monsterHP = 100;
+    },
+    attack: function(){
+      this.didPlayerHeal = false;
+      playerDMG = Math.floor((Math.random() * 10) + 1);
+      monsterDMG = Math.floor((Math.random() * 10) + 1);
+      this.playerHP -= playerDMG;
+      this.monsterHP -= monsterDMG;
+      this.log.push({DamageToPlayer: playerDMG, DamageToMonster: monsterDMG, Heal: 0, SpecialDmg: 0});
+
+      if( playerHP == 0){
+        alert("Game Over");
+      }
+    },
+    specialAttack: function(){
+     this.monsterHP -= 15;
+     this.log.push({DamageToPlayer: 0, DamageToMonster: 0, Heal: 0, SpecialDmg: 15});
+   },
+   heal: function(){
+     this.didPlayerHeal = true;
+     playerHeal = Math.floor((Math.random() * 10) + 1);
+     this.playerHP += playerHeal;
+     this.log.push({DamageToPlayer: 0, DamageToMonster: 0, Heal: playerHeal, SpecialDmg: 0});
+   },
+   giveUp: function(){
+     var willGiveUp = confirm("Do you want to give up?");
+     if(willGiveUp ==  true){
+       location.reload();
+     }else{
+       return false;
+     }
+   }
+  }
+});
